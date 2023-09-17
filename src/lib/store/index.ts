3cda3a01ac2a022575bf124fb194ec06c32f7b9e
Colin <colinclissonhg@gmail.com>
2023-09-17 00:18:09 -0400

    feat: setup store

--- src/lib/store/index.ts
+++ src/lib/store/index.ts
@@ -1,4 +1,12 @@
-import { writable } from "svelte/store";
+import { derived, writable } from "svelte/store";
 import type { Song } from "../../types/song";
 
 export const song = writable<Song|null>(null);
+export const thumbnail = derived(
+    song,
+    ($song) => $song?.thumbnails[0]?.url.split("=w")[0] + "=w500-h500-l90-rj",
+);
+export const artist = derived(
+    song,
+    ($song) => $song?.artists.map((artist: { name: string }) => artist.name).join(" - "),
+);
