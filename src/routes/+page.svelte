54fc50395a4ebd5fa03ac00e7a8641a59da012f6
Colin <colinclissonhg@gmail.com>
2023-09-17 00:17:49 -0400

    feat: music player logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,30 @@
 <script lang="ts">
 	import type { PageData } from "./$types";
 	import SongImage from "../components/SongImage.svelte";
-	import { onMount } from "svelte";
 	import { Button, Search, Spinner } from "flowbite-svelte";
 	import { goto } from "$app/navigation";
-	import { song } from "$lib/store";
+	import { song, thumbnail, artist } from "$lib/store";
+	import MusicPlayer from "../components/MusicPlayer.svelte";
 
 	export let data: PageData;
 
-	let audio: HTMLAudioElement;
 	let search: string;
 	let searching: boolean;
-
-	$: thumbnail = $song?.thumbnails[0]?.url.split("=w")[0] + "=w500-h500-l90-rj";
-	$: artist = $song?.artists.map((artist: { name: string }) => artist.name).join(" - ");
 
 	const searchSong = async () => {
 		if (!search) return;
 		searching = true;
 		await goto(`/?q=${search}`);
 		searching = false;
-		await audio.play();
 	};
 
-	onMount(() => {
+	$: {
 		song.set(data.song);
-		audio.volume = 0.2;
-	});
+	}
 </script>
 
-<section class="dark bg-blue-950 text-white h-full py-16">
-	<div class="max-w-[600px] mx-auto flex flex-col items-center h-full">
+<section class="dark text-white h-full py-12">
+	<div class="max-w-[600px] mx-auto">
 		<div class="w-full mb-12">
 			<Search bind:value={search}>
 				<Button on:click={searchSong}>
@@ -42,10 +36,10 @@
 			</Search>
 		</div>
 		{#if $song}
-			<SongImage image={thumbnail} songName={$song.name} class="mb-5"/>
+			<SongImage image={$thumbnail} songName={$song.name} class="w-[40vh] mx-auto mb-5"/>
 			<h1 class="text-4xl font-bold text-center mb-3">{$song.name}</h1>
-			<p class="text-2xl opacity-70 text-center mb-12">{artist}</p>
-			<audio bind:this={audio} src={$song.url} controls class="flex-1"></audio>
+			<p class="text-2xl opacity-70 text-center mb-5">{$artist}</p>
+			<MusicPlayer />
 		{/if}
 	</div>
 </section>
