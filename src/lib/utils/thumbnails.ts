26546e0d9fc9f8e61bf0e5399e4150bc6be98f9d
Colin <colinclissonhg@gmail.com>
2023-10-11 19:57:26 -0400

    feat: use image proxy

--- src/lib/utils/thumbnails.ts
+++ src/lib/utils/thumbnails.ts
@@ -4,6 +4,7 @@ enum THUMBNAIL_SIZE {
 	LARGE = 500,
 }
 
-export const getThumbnailSize = (thumbnailUrl: string, size: number) => thumbnailUrl.split("=w")[0] + `=w${size}-h${size}-l90-rj`;
+const getImageProxyUrl = (url: string) => `/api/proxy/image?url=${encodeURIComponent(url)}`;
+export const getThumbnailSize = (thumbnailUrl: string, size: number) => getImageProxyUrl(thumbnailUrl.split("=w")[0] + `=w${size}-h${size}-l90-rj`);
 export const getSmallThumbnail = (thumbnailUrl: string) => getThumbnailSize(thumbnailUrl, THUMBNAIL_SIZE.SMALL);
 export const getLargeThumbnail = (thumbnailUrl: string) => getThumbnailSize(thumbnailUrl, THUMBNAIL_SIZE.LARGE);
