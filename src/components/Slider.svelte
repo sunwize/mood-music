9536b3e54527c3caaa8e43521dc87a5f6654f0db
Colin <colinclissonhg@gmail.com>
2023-10-11 18:12:21 -0400

    chore: update slider styling

--- src/components/Slider.svelte
+++ src/components/Slider.svelte
@@ -20,7 +20,8 @@
     on:touchend={onMouseUp}
     role="button"
     tabindex="-1"
-    class={$$props.class}>
+    class={`${$$props.class} input-container`}
+>
     <input
         bind:value={value}
         type="range"
@@ -33,32 +34,55 @@
     />
 </div>
 
-<style>
-	input[type="range"] {
-		-webkit-appearance: none;
-		appearance: none;
-		background: transparent;
-		cursor: pointer;
-		width: 100%;
-		height: 0.5rem;
-	}
+<style lang="scss">
+	.input-container {
+		input[type="range"] {
+			-webkit-appearance: none;
+			appearance: none;
+			background: transparent;
+			cursor: pointer;
+			width: 100%;
+			height: 10px;
 
-	input[type="range"]::-webkit-slider-runnable-track {
-		background: rgba(255, 255, 255, 0.2);
-		background-image: linear-gradient(white, white);
-		background-size: var(--progress) 100%;
-		background-repeat: no-repeat;
-		border-radius: 5px;
-		height: 0.1rem;
-	}
+			&::-webkit-slider-runnable-track {
+				background: rgba(255, 255, 255, 0.2);
+				background-image: linear-gradient(white, white);
+				background-size: var(--progress) 100%;
+				background-repeat: no-repeat;
+				border-radius: 5px;
+				height: 4px;
 
-	input[type="range"]::-webkit-slider-thumb {
-		-webkit-appearance: none;
-		appearance: none;
-		margin-top: -0.3rem;
-		background-color: white;
-		box-shadow: 1px 1px 8px rgba(0, 0, 0, 0.5);
-		width: 0.75rem;
-		height: 0.75rem;
+				@media screen and (max-width: 768px) {
+					background-image: linear-gradient(to left, theme('colors.primary.500'), theme('colors.primary.500'));
+				}
+			}
+
+			&::-webkit-slider-thumb {
+				-webkit-appearance: none;
+				appearance: none;
+				margin-top: -4px;
+				margin-left: -2px;
+				background-color: white;
+				box-shadow: 1px 1px 8px rgba(0, 0, 0, 0.5);
+				width: 0.75rem;
+				height: 0.75rem;
+
+				@media screen and (min-width: 768px) {
+					display: none;
+				}
+			}
+		}
+
+		&:hover {
+			input[type="range"] {
+				&::-webkit-slider-runnable-track {
+					background-image: linear-gradient(to left, theme('colors.primary.500'), theme('colors.primary.500'));
+				}
+
+				&::-webkit-slider-thumb {
+					display: inherit;
+				}
+			}
+		}
 	}
 </style>
